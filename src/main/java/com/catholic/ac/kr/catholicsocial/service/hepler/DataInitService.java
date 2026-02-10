eb2078eaf0835ffc047880a2288e0d013c7e0940
nguyen0220 <teemee202@gmail.com>
2026-02-11 00:08:48 +0900

    .

--- src/main/java/com/catholic/ac/kr/catholicsocial/service/hepler/DataInitService.java
+++ src/main/java/com/catholic/ac/kr/catholicsocial/service/hepler/DataInitService.java
@@ -1,65 +1,65 @@
-package com.catholic.ac.kr.catholicsocial.service.hepler;
-
-import com.catholic.ac.kr.catholicsocial.entity.model.Role;
-import com.catholic.ac.kr.catholicsocial.entity.model.User;
-import com.catholic.ac.kr.catholicsocial.entity.model.UserInfo;
-import com.catholic.ac.kr.catholicsocial.repository.RoleRepository;
-import com.catholic.ac.kr.catholicsocial.repository.UserRepository;
-import com.catholic.ac.kr.catholicsocial.status.Sex;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.Set;
-
-@Service
-@RequiredArgsConstructor
-public class DataInitService {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @PostConstruct
-    public void rolesAndAdminInit() {
-        roleRepository.findByName("ROLE_USER")
-                .orElseGet(() -> {
-                    Role role = new Role();
-                    role.setName("ROLE_USER");
-
-                    return roleRepository.save(role);
-                });
-
-        Role roleAdmin = roleRepository.findByName("ROLE_ADMIN")
-                .orElseGet(() -> {
-                    Role role = new Role();
-                    role.setName("ROLE_ADMIN");
-
-                    return roleRepository.save(role);
-                });
-
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setPassword(passwordEncoder.encode("123456"));
-        admin.setRoles(Set.of(roleAdmin));
-        admin.setLooked(true);
-        admin.setEnabled(true);
-
-        UserInfo adminUserInfo = new UserInfo();
-        adminUserInfo.setFirstName("admin");
-        adminUserInfo.setLastName("admin");
-        adminUserInfo.setPhone("0123456789");
-        adminUserInfo.setEmail("teemee202@gmail.com");
-        adminUserInfo.setBirthday(LocalDate.of(1900, 1, 1));
-        adminUserInfo.setSex(Sex.UNKNOWN);
-
-        admin.setInfo(adminUserInfo);
-
-        userRepository.save(admin);
-
-    }
-}
+//package com.catholic.ac.kr.catholicsocial.service.hepler;
+//
+//import com.catholic.ac.kr.catholicsocial.entity.model.Role;
+//import com.catholic.ac.kr.catholicsocial.entity.model.User;
+//import com.catholic.ac.kr.catholicsocial.entity.model.UserInfo;
+//import com.catholic.ac.kr.catholicsocial.repository.RoleRepository;
+//import com.catholic.ac.kr.catholicsocial.repository.UserRepository;
+//import com.catholic.ac.kr.catholicsocial.status.Sex;
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDate;
+//import java.util.Set;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class DataInitService {
+//    private final UserRepository userRepository;
+//    private final RoleRepository roleRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @PostConstruct
+//    public void rolesAndAdminInit() {
+//        roleRepository.findByName("ROLE_USER")
+//                .orElseGet(() -> {
+//                    Role role = new Role();
+//                    role.setName("ROLE_USER");
+//
+//                    return roleRepository.save(role);
+//                });
+//
+//        Role roleAdmin = roleRepository.findByName("ROLE_ADMIN")
+//                .orElseGet(() -> {
+//                    Role role = new Role();
+//                    role.setName("ROLE_ADMIN");
+//
+//                    return roleRepository.save(role);
+//                });
+//
+//        User admin = new User();
+//        admin.setUsername("admin");
+//        admin.setPassword(passwordEncoder.encode("123456"));
+//        admin.setRoles(Set.of(roleAdmin));
+//        admin.setLooked(true);
+//        admin.setEnabled(true);
+//
+//        UserInfo adminUserInfo = new UserInfo();
+//        adminUserInfo.setFirstName("admin");
+//        adminUserInfo.setLastName("admin");
+//        adminUserInfo.setPhone("0123456789");
+//        adminUserInfo.setEmail("teemee202@gmail.com");
+//        adminUserInfo.setBirthday(LocalDate.of(1900, 1, 1));
+//        adminUserInfo.setSex(Sex.UNKNOWN);
+//
+//        admin.setInfo(adminUserInfo);
+//
+//        userRepository.save(admin);
+//
+//    }
+//}
 
 
 /* .
